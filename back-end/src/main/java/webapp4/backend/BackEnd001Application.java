2968dda8a8460312cac90bf1c33fb902ccb0ff6a
zeroochen <zeroochen12@gmail.com>
2024-03-24 22:59:34 +0800

    add maven setting with spring initializer

---
+++ back-end/src/main/java/webapp4/backend/BackEnd001Application.java
@@ -0,0 +1,13 @@
+package webapp4.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEnd001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEnd001Application.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/webapp4/backend/BackEnd001ApplicationTests.java
@@ -0,0 +1,13 @@
+package webapp4.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEnd001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
